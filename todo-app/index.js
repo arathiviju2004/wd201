8c152e8e9859f405df1e78ae06fe460b0d67032c
arathiviju2004 <arathiviju2004@gmail.com>
2024-10-14 21:31:30 +0530

    todoapp

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,22 +1,5 @@
-const { request } = require('express')
-const express = require('express')
-const app = express()
+const app = require("./app");
 
-app.get("/todos",(request,response)=>{
-    //response.send("helloworld")
-    console.log("Todo List")
-})
-
-app.post("/todos",(request,response)=>{
-    console.log("Creating a todo",request.body)
-})
-app.put("/todos/:id?markAsCompleted",(request,response)=>{
-    console.log("we have to update a todo",request.params.id)
-})
-app.delete("/todos/:id",(request,response)=>{
-    console.log("Delete a todo by id :",request.params.id)
-})
-
-app.listen(3000,()=>{
-    console.log("started express")
-})
+app.listen(3000, () => {
+  console.log("Started express server at port 3000");
+});
